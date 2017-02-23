aa7f337366008b335965f5d233897166f4abb5ed
y-kuronuma <y-kuronuma@imjp.co.jp>
2017-02-23 20:24:03 +0900

    foreach内の書き方変更　preg_quote追加

--- px-files/php/validator/validator.php
+++ px-files/php/validator/validator.php
@@ -19,75 +19,23 @@ class validator{
         foreach( $px->bowl()->get_keys() as $key ){
             $src = $px->bowl()->pull( $key );
 
-            $csv = $px->fs()->read_csv(__DIR__.'/test.csv');
+            $csvs = $px->fs()->read_csv(__DIR__.'/test.csv');
 
             /*$csv  = array();
             $file = __DIR__.'/test.csv';
             $fp   = fopen($file, "r");
-            var_dump( realpath('.'));
-            var_dump( realpath('test.csv'));
 
-            var_dump(__DIR__);
-            var_dump(__FILE__);
-
-            var_dump(__DIR__.'/test.csv');
-
-
-             
             while (($data = fgetcsv($fp, 0, ",")) !== FALSE) {
               $csv[] = $data;
             }
             fclose($fp);*/
 
-            var_dump($csv);
-
-
-            /*$filename = './test.csv';
-            if( ! file_exists( $filename ) ){
-                die("ファイルが存在しません。");
-            }*/
-
-            /*// 文字化け対策
-            // CSVファイルの中身をすべて取り出す
-            $tmpCSV = file_get_contents( $filename );
-            // 文字コードをUTF-8に変換
-            $tmpCSV = mb_convert_encoding( $tmpCSV, 'UTF-8', 'CP932' );
-            // 一時ファイルに保存
-            $fp = tmpfile();
-            fwrite( $fp, $tmpCSV );
-            // ファイルポインタを先頭に戻す
-            rewind( $fp );
-            // ロケールを設定
-            setlocale( LC_ALL, 'ja_JP, UTF-8' );
-
-            // 1行づつ読み込み
-            $lines = fgetcsv( $fp );
-            list( $keywords, $errormese ) = $lines;
-            $px->error( htmlspecialchars( "エラー${keywords}が発生しました！" ) );*/
-
-            /*while( $arr = fgetcsv( $fp ) ){
-                if( ! array_diff( $arr, array('') ) ){ // 空行を除外
-                    continue;
-                }
-                list( $keywords, $errormese ) = $arr;
-                $px->error( $arr[0] );
-                if( preg_match( $keywords, $src )){
-                    $px->error( htmlspecialchars( "エラー${errormese}が発生しました！" ) );
-                }
-            }*/
-
-            /*foreach ($lines as $key => $line) {
-                $errormese = explode(',', $line);
-                if( preg_match($errormese[0], $src)){
-                    $px->error( htmlspecialchars("エラー${errormese[1]}が発生しました！") );
-                }
-            }*/
-
-            /*//ファイルロックを解除
-            fflush( $fp );
-            flock( $fp, LOCK_UN) ;
-            //ファイルを閉じる
-            fclose( $fp );*/
+            //foreach ($csvs as $key => $csv) {
+            foreach ($csvs as $csv) {
+                if( preg_match( '/'.preg_quote($csv[0]).'/', $src )){
+                    $px->error( htmlspecialchars( "エラー${csv[1]}が発生しました！" ) );
+                };
+            };
 
             $px->bowl()->replace( $src, $key );
         }
